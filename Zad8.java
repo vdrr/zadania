dc9478176ce0c68d952dd225256ecc041fb1b5c7
vdrr <darosoro@gmail.com>
2018-05-10 19:59:20 +0200

    Suma ciągu liczb od A do B

---
+++ Zad8.java
@@ -0,0 +1,43 @@
+package pl.sdacademy.zadania;
+
+import java.util.Scanner;
+
+public class Zad8 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Podaj liczbe A: ");
+        int a = sc.nextInt();
+
+        System.out.println("Podaj liczbe B (B>A): ");
+        int b = sc.nextInt();
+
+        if (a<b) {
+            int sum = 0;
+            int i = a;
+
+            do {
+                sum += i;
+                i++;
+            } while (i <= b);
+            System.out.println(sum);
+
+            i = a;
+            sum = 0;
+            while (i <= b) {
+                sum += i;
+                i++;
+            }
+            System.out.println(sum);
+
+            sum = 0;
+            for (i = a; i <= b; i++) {
+                sum += i;
+            }
+            System.out.println(sum);
+        } else {
+            System.out.println("Error: Invalid value of B!");
+        }
+    }
+}
